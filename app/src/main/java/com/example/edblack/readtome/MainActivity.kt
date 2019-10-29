ea74c3f9c2e3e255cd3b79ebb35e092b58a686d4
Edilson Ngulele <edilsonngulele@gmail.com>
2019-10-29 16:44:05 +0200

    chore: migrate to androidx

--- app/src/main/java/com/example/edblack/readtome/MainActivity.kt
+++ app/src/main/java/com/example/edblack/readtome/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.edblack.readtome
 
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
